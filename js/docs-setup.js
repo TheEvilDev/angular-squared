9925a4c54f3eee7bf91f9f022d66ec8084639552
Mitchell Lee <mitchlee@amazon.com>
2015-01-24 16:25:52 -0800

    Built angular-squared from commit 7df9bcc on branch master

---
+++ js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angular-squared",
+      "shortName": "angular-squared",
+      "type": "directive",
+      "moduleName": "angular-squared",
+      "shortDescription": "Containing module for all things squared!",
+      "keywords": "angular-squared api directive module squared things"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
